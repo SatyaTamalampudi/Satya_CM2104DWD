88a9cd01050155babd89edd42ba2f9bf7c54dfb5
Satya <tetalasatya123@gmail.com>
2018-04-11 14:39:09 +0100

    lab13

--- week7/server.js
+++ week7/server.js
@@ -4,5 +4,9 @@ var app = express();
 app.get('/',function(req,res){
   res.send("Hello World! by express");
 });
+app.get('/test',function(req,res){
+  res.send("this is route 2");
+});
+
 
 app.listen(8080);
