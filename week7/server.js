ab8079055a9fa1a6eb7a25717eb931386cd54885
Satya <tetalasatya123@gmail.com>
2018-04-11 14:50:00 +0100

    lab13/joke

--- week7/server.js
+++ week7/server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var knockknock = require('knock-knock-jokes');
 
 app.get('/',function(req,res){
   res.send("Hello World! by express");
 });
 app.get('/test',function(req,res){
   res.send("this is route 2");
+});
+app.get('/joke',function(req,res){
+res.writeHead(200, {'Content-Type':'text/html'});
+var randomJoke = knockknock()
+res.end(randomJoke);
+  
 });
 
 
