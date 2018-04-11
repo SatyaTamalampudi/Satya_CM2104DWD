fc3fbd156f86407d2b30d201d2d3604c6c2da17b
Satya <tetalasatya123@gmail.com>
2018-04-11 15:06:20 +0100

    lab13

--- week7/server.js
+++ week7/server.js
@@ -12,8 +12,12 @@ app.get('/joke',function(req,res){
 res.writeHead(200, {'Content-Type':'text/html'});
 var randomJoke = knockknock()
 res.end(randomJoke);
-  
 });
+app.get('/add',function(req,res){
+  var x = req.query.x;
+  var y = req.query.y;
+  res.send("X + Y="+(X+Y))
 
+});
 
 app.listen(8080);
