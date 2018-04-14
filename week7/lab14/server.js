c53367c1d9533afc8dcce3972cb06991c1c9137d
Satya <tetalasatya123@gmail.com>
2018-04-14 10:46:46 +0100

    update

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var twitter = require('twitter');
 var client = new twitter({
-  consumer_key = '4mkqqMLHm3ZznBf18bPZHGeSD',
-  consumer_secret = 'AX45ncSR4vduUknCLc7fAlK00pKf49S92I8VM7cC47Fw4Vof5m',
-  access_token_key = '2580230261-iW5oVJQhFkgdtqDiPoUhqgrvhRjZmOMgcFNO9yY',
-  access_token_secret = 'TOXzdBuA9gb0hCBW5B4AukUVbQZfi3vAtJz3adj4LhkQQ'
+  consumer_key: '4mkqqMLHm3ZznBf18bPZHGeSD',
+  consumer_secret: 'AX45ncSR4vduUknCLc7fAlK00pKf49S92I8VM7cC47Fw4Vof5m',
+  access_token_key: '2580230261-iW5oVJQhFkgdtqDiPoUhqgrvhRjZmOMgcFNO9yY',
+  access_token_secret: 'TOXzdBuA9gb0hCBW5B4AukUVbQZfi3vAtJz3adj4LhkQQ'
 })
 var parms = (screen_name,'nodejs'),
 
