49e9dfb6d321956b2a56b04a605e91608005e2c8
Satya <tetalasatya123@gmail.com>
2018-04-14 07:38:18 +0100

    tweets retrieve

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var twitter = require('twitter');
+var client = new twitter({
+  consumer_key = '4mkqqMLHm3ZznBf18bPZHGeSD',
+  consumer_secret = 'AX45ncSR4vduUknCLc7fAlK00pKf49S92I8VM7cC47Fw4Vof5m',
+  access_token_key = '2580230261-iW5oVJQhFkgdtqDiPoUhqgrvhRjZmOMgcFNO9yY',
+  access_token_secret = 'TOXzdBuA9gb0hCBW5B4AukUVbQZfi3vAtJz3adj4LhkQQ'
+})
+var parms = (screen_name,'nodejs'),
 
  app.use(express.static('public'))
  app.get('/', function(req, res){
+   client.get('statuses/user_timeline',parms,function(error,tweets,response){
+     if(!error){
+       console.log(tweets);
+     }
+   });
       res.send("Hello world! by express");
      });
       app.listen(8080);
