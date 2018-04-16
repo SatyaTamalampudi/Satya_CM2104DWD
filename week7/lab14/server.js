59edd39f080e921e72e7725728c1d0eaf76eb3ae
Satya <tetalasatya123@gmail.com>
2018-04-16 10:36:20 +0100

    twitter

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -6,11 +6,12 @@ var client = new twitter({
   consumer_secret: 'AX45ncSR4vduUknCLc7fAlK00pKf49S92I8VM7cC47Fw4Vof5m',
   access_token_key: '2580230261-iW5oVJQhFkgdtqDiPoUhqgrvhRjZmOMgcFNO9yY',
   access_token_secret: 'TOXzdBuA9gb0hCBW5B4AukUVbQZfi3vAtJz3adj4LhkQQ'
-})
-var parms = (screen_name,'nodejs'),
+});
 
- app.use(express.static('public'))
- app.get('/', function(req, res){
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+   var parms = (screen_name,'nodejs'),
    client.get('statuses/user_timeline',parms,function(error,tweets,response){
      if(!error){
        console.log(tweets);
